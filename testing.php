0923b73fe38cfbdf72a7e56793bfdc0a652738f1
km <km.bitfarm@gmail.com>
2012-04-16 13:55:21 +0200

    First commit

---
+++ testing.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Bitfarm
+ * Date: 16.04.12
+ * Time: 13:37
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Hello world.";
+
+?>
